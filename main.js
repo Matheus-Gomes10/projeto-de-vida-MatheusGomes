21c32f15bed56cedae60b98bf38af0150ec1624a
Matheus-Gomes10 <matheus.gomes21@escola.pr.gov.br>
2025-06-17 07:52:36 -0300

    aasawa

--- main.js
+++ main.js
@@ -1,6 +1,19 @@
 const botoes = document.querySelectorAll(".botao");
 const textos = document.querySelectorAll(".aba-conteudo");
 
-for () {
-    
+for (let i=0; i<botoes.length; j++) {
+    botoes[i].onclick = function(){
+        for(let j=0; j<botoes.length; j++){
+            botoes[j].classList.remove("ativo")
+            textos[i].classList.remove("ativo")
+        }
+        botoes[i].classList.add("ativo");
+        textos[i].classList.add("ativo");
+    }
 }
+
+const contadores = document.querySelectorAll(".contador")
+const tempoObjetivo1 = new Date("")
+let tempoAtual = new Date();
+
+contadores[0].textContent = tempoObjetivo1 - tempoAtual;
